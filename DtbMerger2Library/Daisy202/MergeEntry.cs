b7f203e29a9d1c2d5366d1c25ab35a67e0646477
Ole Holst Andersen <OHA@nota.dk>
2018-05-16 08:44:52 +0200

    MergeEntry partially finished Only GetTestElements need implementation

--- DtbMerger2Library/Daisy202/MergeEntry.cs
+++ DtbMerger2Library/Daisy202/MergeEntry.cs
@@ -1,30 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Xml.Linq;
+using System.Xml.XPath;
 
 namespace DtbMerger2Library.Daisy202
 {
     public class MergeEntry
     {
+        private static readonly String[] HeadingLocalNames = new[] {"h1", "h2", "h3", "h4", "h5", "h6"};
+
+        private XDocument ncc, smil;
         public Uri SourceNavEntry { get; set; }
+
+        public XDocument Ncc
+        {
+            get
+            {
+                if (ncc == null)
+                {
+                    if (SourceNavEntry == null)
+                    {
+                        throw new InvalidOperationException("SourceNavEntry Uri is null");
+                    }
+                    if (!SourceNavEntry.IsAbsoluteUri)
+                    {
+                        throw new InvalidOperationException("SourceNavEntry Uri must be absolute");
+                    }
+                    ncc = XDocument.Load(Uri.UnescapeDataString(SourceNavEntry.AbsolutePath), LoadOptions.SetBaseUri|LoadOptions.SetLineInfo);
+                }
+                return ncc;
+            }
+        }
+
+        public XDocument Smil
+        {
+            get
+            {
+                if (smil == null)
+                {
+                    try
+                    {
+                        smil = GetNccElements()
+                            .SelectMany(e =>
+                                e.Descendants(e.Name.Namespace + "a").Select(a => a.Attribute("href")?.Value.ToLowerInvariant()))
+                            .Distinct()
+                            .Where(href => !String.IsNullOrEmpty(href))
+                            .Select(href => 
+                                XDocument.Load(
+                                    Uri.UnescapeDataString(new Uri(new Uri(ncc.BaseUri), href).AbsolutePath), 
+                                    LoadOptions.SetBaseUri | LoadOptions.SetLineInfo))
+                            .SingleOrDefault();
+                    }
+                    catch (InvalidOperationException e)
+                    {
+                        throw new InvalidOperationException("NCC elements of MergeEntry links to multiple smil files", e);
+                    }
+                }
+                return smil;
+            }
+        }
 
         public IEnumerable<XElement> GetNccElements()
         {
-            if (SourceNavEntry == null)
-            {
-                throw new InvalidOperationException("SourceNavEntry Uri is null");
-            }
-            if (!SourceNavEntry.IsAbsoluteUri)
-            {
-                throw new InvalidOperationException("SourceNavEntry Uri must be absolute");
-            }
-            if (String.IsNullOrEmpty(SourceNavEntry.Fragment))
+            if (String.IsNullOrEmpty(SourceNavEntry?.Fragment))
             {
                 throw new InvalidOperationException($"SourceNavEntry Uri {SourceNavEntry} has no fragment");
             }
-            var heading = XDocument.Load(SourceNavEntry.AbsoluteUri, LoadOptions.SetBaseUri)
+            var heading = Ncc
                 .Descendants()
                 .FirstOrDefault(e => $"#{e.Attribute("id")?.Value}" == SourceNavEntry.Fragment);
             if (heading == null)
@@ -33,7 +78,7 @@ namespace DtbMerger2Library.Daisy202
             }
             var res = new List<XElement>() {heading};
             var next = heading.ElementsAfterSelf().FirstOrDefault();
-            while (next != null && !(new[] { "h1", "h2", "h3", "h4", "h5", "h6" }.Contains(next.Name.LocalName)))
+            while (next != null && !(HeadingLocalNames.Contains(next.Name.LocalName)))
             {
                 res.Add(next);
                 next = next.ElementsAfterSelf().FirstOrDefault();
@@ -41,9 +86,70 @@ namespace DtbMerger2Library.Daisy202
             return res;
         }
 
+        public static XElement GetReferencedElement(XDocument destDoc, XAttribute uriAttr)
+        {
+            if (uriAttr == null)
+            {
+                return null;
+            }
+            var destDocUri = new Uri(destDoc.BaseUri);
+            var uri = new Uri(new Uri(uriAttr.BaseUri), uriAttr.Value);
+            if (!String.Equals(destDocUri.AbsolutePath, uri.AbsolutePath, StringComparison.InvariantCultureIgnoreCase) )
+            {
+                return null;
+            }
+            if (String.IsNullOrEmpty(uri.Fragment))
+            {
+                return null;
+            }
+            return destDoc.XPathSelectElement($"//*[@id='{uri.Fragment.TrimStart('#')}']");
+        }
+
+        private XElement GetSmilPar(XElement elem)
+        {
+            var hrefAttr = elem
+                .Descendants()
+                .FirstOrDefault(e => e.Name.LocalName == "a")
+                ?.Attribute("href");
+            if (hrefAttr == null)
+            {
+                return null;
+            }
+            var par = GetReferencedElement(Smil, hrefAttr);
+            if (par?.Name.LocalName == "text")
+            {
+                par = par.Parent;
+            }
+
+            return par;
+        }
+
         public IEnumerable<XElement> GetSmilElements()
         {
-            return new XElement[0];
+            if (Smil == null)
+            {
+                return new XElement[0];
+            }
+            var nccElements = GetNccElements().ToList();
+            var firstPar = GetSmilPar(nccElements.First());
+            if (firstPar == null)
+            {
+                throw new InvalidOperationException("First ncc element does not point to smil par");
+            }
+            var nextHeadingPar = GetSmilPar(
+                nccElements.Last().ElementsAfterSelf().FirstOrDefault(e => HeadingLocalNames.Contains(e.Name.LocalName)));
+            var res = new List<XElement>();
+            while (firstPar != null)
+            {
+                if (firstPar == nextHeadingPar)
+                {
+                    break;
+                }
+                res.Add(firstPar);
+                firstPar = firstPar.ElementsAfterSelf().FirstOrDefault(e => e.Name.LocalName == "par");
+            }
+
+            return res;
         }
 
         public IEnumerable<XElement> GetTextElements()
@@ -53,7 +159,55 @@ namespace DtbMerger2Library.Daisy202
 
         public IEnumerable<AudioSegment> GetAudioSegments()
         {
-            return new AudioSegment[0];
+            var audios = GetSmilElements()
+                .SelectMany(par => par.Descendants(par.Name.Namespace + "audio"))
+                .ToList();
+            int i = 0;
+            while (i < audios.Count - 1)
+            {
+                var a1 = audios[i];
+                var a2 = audios[i + 1];
+                if (a1.Attribute("src")?.Value == a2.Attribute("src")?.Value)
+                {
+                    if (a1.Attribute("clip-end")?.Value == a2.Attribute("clip-begin")?.Value)
+                    {
+                        a1.SetAttributeValue("clip-end", a2.Attribute("clip-end")?.Value);
+                        audios.Remove(a2);
+                        continue;
+                    }
+                }
+                i++;
+            }
+
+            return audios
+                .Where(audio => !String.IsNullOrEmpty(audio.Attribute("src")?.Value))
+                .Select(audio => new AudioSegment()
+                {
+                    AudioFile = new Uri(new Uri(audio.BaseUri), audio.Attribute("src")?.Value??""),
+                    ClipBegin = ParseSmilClip(audio.Attribute("clip-begin")?.Value),
+                    ClipEnd = ParseSmilClip(audio.Attribute("clip-end")?.Value)
+                });
+        }
+
+        private static TimeSpan ParseSmilClip(string val)
+        {
+            if (String.IsNullOrWhiteSpace(val))
+            {
+                return TimeSpan.MinValue;
+            }
+            val = val.Trim();
+            if (val.StartsWith("npt=") && val.EndsWith("s"))
+            {
+                var secs = Double.Parse(
+                    val.Substring(4, val.Length - 5), 
+                    NumberStyles.Float,
+                    CultureInfo.InvariantCulture);
+                return TimeSpan.FromSeconds(secs);
+            }
+            else
+            {
+                throw new ArgumentException($"Value {val} is not a valid Daisy 2.02 smil clop value");
+            }
         }
 
         public IEnumerable<MediaEntry> GetMediaEntries()
--- DtbMerger2LibraryTests/Daisy202/MergeEntryTests.cs
+++ DtbMerger2LibraryTests/Daisy202/MergeEntryTests.cs
@@ -33,5 +33,45 @@ namespace DtbMerger2LibraryTests.Daisy202
             Assert.IsFalse(String.IsNullOrEmpty(nccElements.First().BaseUri), "First ncc element has no base uri");
 
         }
+
+        [TestMethod]
+        public void GetSmilElementsTest()
+        {
+            var entry = new MergeEntry() { SourceNavEntry = new UriBuilder(dtb1NccUri) { Fragment = "nav1" }.Uri };
+            var smilElements = entry.GetSmilElements()?.ToList();
+            Assert.IsNotNull(smilElements, "Smil elements null");
+            Assert.IsTrue(smilElements.Any(), "Found no smil elements");
+            Assert.IsTrue(smilElements.All(e => e.Name.LocalName == "par"));
+        }
+
+        [TestMethod]
+        public void GetAudioSegmentsTest()
+        {
+            var entry = new MergeEntry() { SourceNavEntry = new UriBuilder(dtb1NccUri) { Fragment = "nav1" }.Uri };
+            var audioSegments = entry.GetAudioSegments().ToList();
+            Assert.IsNotNull(audioSegments, "Audio segments elements null");
+            Assert.IsTrue(audioSegments.Any(), "Found no audio segments");
+            Assert.AreEqual(1, audioSegments.Count(), "Expected one audio segment in DTB1 first heading");
+        }
+
+        [TestMethod]
+        public void NccTest()
+        {
+            var entry = new MergeEntry() { SourceNavEntry = new UriBuilder(dtb1NccUri) { Fragment = "nav1" }.Uri };
+            Assert.IsNotNull(entry.Ncc, "Entry has null Ncc");
+            Assert.IsNotNull(entry.Ncc.BaseUri, "Entry Ncc has null BaseUri");
+            Assert.IsTrue(Uri.IsWellFormedUriString(Uri.EscapeUriString(entry.Ncc.BaseUri), UriKind.Absolute), $"Entrys Ncc does not have a well formed absolute BaseUri: {entry.Ncc.BaseUri}");
+            Assert.AreEqual("html", entry.Ncc.Root?.Name?.LocalName);
+        }
+
+        [TestMethod]
+        public void SmilTest()
+        {
+            var entry = new MergeEntry() { SourceNavEntry = new UriBuilder(dtb1NccUri) { Fragment = "nav1" }.Uri };
+            Assert.IsNotNull(entry.Smil, "Entry has null Smil");
+            Assert.IsNotNull(entry.Smil.BaseUri, "Entry Smil has null BaseUri");
+            Assert.IsTrue(Uri.IsWellFormedUriString(Uri.EscapeUriString(entry.Smil.BaseUri), UriKind.Absolute), $"Entrys Ncc does not have a well formed absolute BaseUri: {entry.Ncc.BaseUri}");
+            Assert.AreEqual("smil", entry.Smil.Root?.Name?.LocalName);
+        }
     }
 }
