a35f5173efd675877259728e67162018f5c20670
Ole Holst Andersen <OHA@nota.dk>
2018-10-21 22:39:00 +0200

    Upped version to 1.0.3 (deterministic style)

--- Application/DtbSynthesizer/DtbSynthesizerGui/Properties/AssemblyInfo.cs
+++ Application/DtbSynthesizer/DtbSynthesizerGui/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DtbSynthesizerGui")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Application for generating Digital Talking Books using synthetic speech")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DtbSynthesizerGui")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © Nota 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +31,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.3")]
